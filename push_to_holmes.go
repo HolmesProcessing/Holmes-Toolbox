4774432bf09f35b594c58cbd07116ecc1da3b65f
Marcel Schumacher <ga25hac@mytum.de>
2016-08-17 15:44:27 +0200

    fused scripts for object upload and tasking

--- push_to_holmes.go
+++ push_to_holmes.go
@@ -20,6 +20,11 @@ import (
 
 	"github.com/rakyll/magicmime"
 	"strings"
+	"golang.org/x/crypto/ssh/terminal"
+	"encoding/json"
+	"fmt"
+	"net/url"
+	"strconv"
 )
 
 type critsSample struct {
@@ -27,10 +32,19 @@ type critsSample struct {
 	MD5 string        `json:"md5"`
 }
 
+type Task struct {
+	PrimaryURI     string              `json:"primaryURI"`
+	SecondaryURI   string              `json:"secondaryURI"`
+	Filename       string              `json:"filename"`
+	Tasks          map[string][]string `json:"tasks"`
+	Tags           []string            `json:"tags"`
+	Attempts       int                 `json:"attempts"`
+	Source         string              `json:"source"`
+}
+
 var (
 	client          *http.Client
 	critsFileServer string
-	holmesStorage   string
 	directory       string
 	comment         string
 	userid          string
@@ -42,6 +56,14 @@ var (
 	numWorkers      int
 	wg              sync.WaitGroup
 	c               chan string
+
+	fPath      string
+	tasks      string
+	tags       string
+	gatewayURI string
+	username   string
+	password   string
+	tasking    bool
 )
 
 func init() {
@@ -70,22 +92,110 @@ func main() {
 	log.Println("Running...")
 
 	// cmd line flags
-	var fPath string
 	flag.StringVar(&fPath, "file", "", "List of samples (MD5, SHAX, CRITs ID) to upload. Files are first searched locally. If they are not found and a CRITs file server is specified, they are taken from there. (optional)")
-	flag.StringVar(&critsFileServer, "cfs", "", "Full URL to your CRITs file server, as a fallback (optional)")
-	flag.StringVar(&holmesStorage, "storage", "", "Full URL to your Holmes-Storage server, i.e. 'http://storage:8080', or your Master-Gateway (with https)")
-	flag.StringVar(&mimetypePattern, "mime", "", "Only upload files with the specified mime-type (as substring)")
-	flag.StringVar(&directory, "dir", "", "Directory of samples to upload")
 	flag.StringVar(&comment, "comment", "", "Comment of submitter")
 	flag.StringVar(&source, "src", "", "Source information for the files")
+	flag.BoolVar(&insecure, "insecure", false, "If set, disables certificate checking")
+	flag.BoolVar(&tasking, "tasking", false, "Specify whether to do sample upload or tasking")
+	flag.StringVar(&username, "user", "", "Your username for authenticating to the master-gateway.")
+	flag.StringVar(&password, "pw", "", "Your password for authenticating to the master-gateway. If this value is not set, you will be prompted for it.")
+	flag.StringVar(&gatewayURI, "gateway", "", "The URI of the master-gateway.")
+	flag.StringVar(&tags, "tags", "", "The tags for these tasks.")
+
+	// object specific
+	flag.StringVar(&critsFileServer, "cfs", "", "Full URL to your CRITs file server, as a fallback (optional)")
+	flag.StringVar(&mimetypePattern, "mime", "", "Only upload files with the specified mime-type (as substring)")
+	flag.StringVar(&directory, "dir", "", "Directory of samples to upload")
 	flag.StringVar(&userid, "uid", "-1", "User ID of submitter")
 	flag.IntVar(&numWorkers, "workers", 1, "Number of parallel workers")
 	flag.BoolVar(&recursive, "rec", false, "If set, the directory specified with \"-dir\" will be iterated recursively")
-	flag.BoolVar(&insecure, "insecure", false, "If set, disables certificate checking")
+
+	// tasking specific
+	flag.StringVar(&tasks, "tasks", "", "The tasks to execute.")
+
 	flag.Parse()
 
-	//log.Sprintf("Copying samples from %s", fPath)
+	if password == "" {
+		println("Please input your password for the master-gateway: ")
+		pw, err := terminal.ReadPassword(0)
+		if err != nil {
+			log.Fatal(err)
+		}
+		password = string(pw)
+	}
 
+	if tasking {
+		main_tasking()
+	} else {
+		main_object()
+	}
+}
+
+func main_tasking() {
+	log.Println("Tasking...")
+	allTasks := make([]Task,0)
+	file, err := os.Open(fPath)
+	if err != nil {
+		log.Println("Couln't open file containing sample list!", err.Error())
+		return
+	}
+	defer file.Close()
+
+
+	scanner := bufio.NewScanner(file)
+	scanner.Split(bufio.ScanLines)
+	task := &Task{PrimaryURI:"", SecondaryURI:"", Filename:"", Tasks:nil, Tags:nil, Attempts:0, Source:""}
+	err = json.Unmarshal([]byte(tasks), &task.Tasks)
+	if err != nil {
+		log.Fatal(err)
+	}
+	err = json.Unmarshal([]byte(tags), &task.Tags)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// line by line
+	for scanner.Scan() {
+		t := scanner.Text()
+		fmt.Sscanf(t, "%s %s %s", &task.PrimaryURI, &task.Filename, &task.Source)
+		allTasks = append(allTasks, *task)
+	}
+	jsoned, err := json.Marshal(allTasks)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	log.Printf("%+v\n", string(jsoned))
+	data := &url.Values{}
+	data.Set("task", string(jsoned))
+	data.Add("username", username)
+	data.Add("password", password)
+	
+	req, err := http.NewRequest("POST", gatewayURI, bytes.NewBufferString(data.Encode()))
+	req.Header.Add("Content-Type", "application/x-www-form-urlencoded")
+	req.Header.Add("Content-Length", strconv.Itoa(len(data.Encode())))
+	tr := &http.Transport{}
+	if insecure{
+		// Disable SSL verification
+		tr = &http.Transport{
+			TLSClientConfig: &tls.Config{InsecureSkipVerify: true},
+		}
+	}
+	client := &http.Client{Transport: tr}
+	resp, err := client.Do(req)
+	if err != nil {
+		log.Fatal("Error: ", err)
+	}
+	tskerrors, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		log.Fatal("Error: ", err)
+	}
+	log.Println("The server returned:")
+	log.Println(tskerrors)
+}
+
+func main_object() {
+	log.Println("Object Uploading...")
 	c = make(chan string)
 	for i := 0; i < numWorkers; i++ {
 		go worker()
@@ -160,15 +270,17 @@ func walkFn(path string, fi os.FileInfo, err error) (error) {
 func copySample(name string) {
 	// set all necessary parameters
 	parameters := map[string]string{
-		"user_id": userid, // user id of uploader (command line argument)
-		"source":  source, // (TODO) Gateway should match existing sources (command line argument)
-		"name":    filepath.Base(name), // filename
-		"date":    time.Now().Format(time.RFC3339),
-		"comment": comment, // comment from submitter (command line argument)
-		//"tags"
+		"user_id":  userid, // user id of uploader (command line argument)
+		"source":   source, // (TODO) Gateway should match existing sources (command line argument)
+		"name":     filepath.Base(name), // filename
+		"date":     time.Now().Format(time.RFC3339),
+		"comment":  comment, // comment from submitter (command line argument)
+		"tags":     tags,
+		"username": username,
+		"password": password,
 	}
 
-	request, err := buildRequest(holmesStorage+"/samples/", parameters, name)
+	request, err := buildRequest(gatewayURI+"/samples/", parameters, name)
 
 	if err != nil {
 		log.Fatal("ERROR: " + err.Error())
